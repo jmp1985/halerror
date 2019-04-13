a919b3134e5d0e87988a245bd02c66bd212ca585
James Parkhurst <james.parkhurst@diamond.ac.uk>
2019-04-13 20:17:03 +0100

    Move metadata to cfg file

--- setup.py
+++ setup.py
@@ -4,28 +4,19 @@
 # This code is distributed under the BSD license.
 #
 from setuptools import setup
-from os.path import abspath
-from os.path import dirname
-from os.path import join
 
-def get_readme():
-    directory = abspath(dirname(__file__))
-    with open(join(directory, 'README.md'), encoding='utf-8') as readme:
-        return readme.read()
+def main():
+    """
+    Setup the package
 
-setup(
-    name="halerror",
-    version="0.1",
-    author="James Parkhurst",
-    author_email="james.parkhurst@diamond.ac.uk",
-    description="The best Python exception library!",
-    long_description=get_readme(),
-    license="BSD",
-    keywords="HAL-9000",
-    packages=["halerror"],
-    install_requires=[],
-    setup_requires = ["pytest-runner"],
-    tests_require = ["pytest", "pytest-cov", "mock"],
-    test_suite = "tests",
-    classifiers=["License :: OSI Approved :: BSD License"],
-)
+    """
+    setup(
+        packages=["halerror"],
+        install_requires=[],
+        setup_requires=["pytest-runner"],
+        tests_require=["pytest", "pytest-cov", "mock"],
+        test_suite="tests",
+    )
+
+if __name__ == '__main__':
+    main()
